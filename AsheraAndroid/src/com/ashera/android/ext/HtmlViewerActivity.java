8a57f5f0f1b4a9eabecc936d48a99c86b54faa75
ramm <ram.muralidhar@gmail.com>
2015-07-05 13:46:23 +0530

    Added hml view activity.

---
+++ AsheraAndroid/src/com/ashera/android/ext/HtmlViewerActivity.java
@@ -0,0 +1,12 @@
+package com.ashera.android.ext;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HtmlViewerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+}
